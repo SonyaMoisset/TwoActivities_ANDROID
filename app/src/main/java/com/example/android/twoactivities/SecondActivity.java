e5b61984b69eb8d4ecc37f63121351e6d7687c25
smoisset <smoisset@worldremit.com>
2018-01-14 17:48:59 +0000

    Add lifecycle methods to SecondActivity

--- app/src/main/java/com/example/android/twoactivities/SecondActivity.java
+++ app/src/main/java/com/example/android/twoactivities/SecondActivity.java
@@ -3,12 +3,13 @@ package com.example.android.twoactivities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
-
+    private static final String LOG_TAG = SecondActivity.class.getSimpleName();
     public static final String EXTRA_REPLY = "com.example.android.twoactivities.extra.REPLY";
 
     private EditText mReply;
@@ -26,12 +27,49 @@ public class SecondActivity extends AppCompatActivity {
         textView.setText(message);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
+    }
+
     public void returnReply(View view) {
         Intent replyIntent = new Intent();
         String reply = mReply.getText().toString();
 
         replyIntent.putExtra(EXTRA_REPLY, reply);
         setResult(RESULT_OK, replyIntent);
+        Log.d(LOG_TAG, "End SecondActivity");
         finish();
     }
 }
